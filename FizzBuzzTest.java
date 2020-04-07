bd3d23c725d0ec454296b13c7268531159014307
frances <frncsbthn@gmail.com>
2020-04-07 12:16:48 -0700

    fizzbuzz

---
+++ FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fB = new FizzBuzz();
+        String fizzy = fB.fizzBuzz(2);
+        System.out.println(fizzy);
+    }
+}
